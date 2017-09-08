a579bdd403bd2405af4da7513fc339b444cf88bf
Daniel Henry <email378637@gmail.com>
2017-09-07 17:22:47 -0700

    Update robotArm.py

--- robotControl/python_interface/robotArm.py
+++ robotControl/python_interface/robotArm.py
@@ -2,25 +2,11 @@ import serial, time
 from random import randint
 
 try:
-    ser = serial.Serial('COM3', 9600, timeout=.8)
+    ser = serial.Serial('COM3', 9600, timeout=1)
 except Exception as e:
     print(e)
     
 time.sleep(2)
-##while True:
-##    movements = {'fwrd' : str(randint(65, 180)), 'base' : str(randint(0, 180)), 'claw' : str(randint(135, 180))}
-##    for key, value in movements.items():
-##        if key == 'base':
-##            if(int(movements['fwrd']) > 130):
-##                break
-##        key += '\r\n'
-##        value += '\r\n'
-##        ser.write(key.encode())
-##        print('Ardunio output: ', ser.readline().decode())
-##        time.sleep(.5)
-##        ser.write(value.encode())
-##        print('Ardunio output: ', ser.readline().decode())
-##        time.sleep(.5)
 
 while True:
     inputs = (input('Enter servo: '), input('Enter angle: '))
