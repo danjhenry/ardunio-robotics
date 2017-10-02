d9ef18cf30a17f67447091b5be1234f7ee9b5846
Daniel Henry <email378637@gmail.com>
2017-10-02 10:46:25 -0700

    Update rc_Logic.ino

--- robotControl/rc_logic/rc_Logic.ino
+++ robotControl/rc_logic/rc_Logic.ino
@@ -1,7 +1,6 @@
-#include <ArduinoSTL.h>
 #include <Arduino.h>
 
-using namespace std;
+bool on = false;
 const int fwrd = 2;
 const int back = 3;
 const int rght = 4;
@@ -17,42 +16,39 @@ void setup() {
 }
 
 void loop() {
-  string movement;
-  string check;
-  int pause;
-  while (true) {
-    cin >> check;
-    cout << check << endl;
-    if (check == "noInput") {
+  char movement;
+  if (Serial.available() > 0) {
+    movement = Serial.read();
+    if (movement == 'q' and on == true) {
       for (int i = 0; i < 4; i++) {
         digitalWrite(pins[i], LOW);
       }
+      on = false;
     }
-    else {
-      break;
+    else if (movement == 'w') {
+      on = true;
+      moveMotor(fwrd);
     }
-  }
-  cout << "enter motor to move(fwrd, back, left, right, fwrdleft, fwrdright): ";
-  cin >> movement;
-  cout << movement << endl;
-
-  if (movement == "fwrd") {
-    moveMotor(fwrd);
-  }
-  else if (movement == "back") {
-    moveMotor(back);
-  }
-  else if (movement == "left") {
-    moveMotor(lft);
-  }
-  else if (movement == "right") {
-    moveMotor(rght);
-  }
-  else if (movement == "fwrdleft") {
-    moveMotorComb(lft, fwrd);
-  }
-  else if (movement == "fwrdright") {
-    moveMotorComb(rght, fwrd);
+    else if (movement == 's') {
+      on = true;
+      moveMotor(back);
+    }
+    else if (movement == 'a') {
+      on = true;
+      moveMotor(lft);
+    }
+    else if (movement == 'd') {
+      on = true;
+      moveMotor(rght);
+    }
+    else if (movement == 'l') {
+      on = true;
+      moveMotorComb(lft, fwrd);
+    }
+    else if (movement == 'r') {
+      on = true;
+      moveMotorComb(rght, fwrd);
+    }
   }
 }
 
