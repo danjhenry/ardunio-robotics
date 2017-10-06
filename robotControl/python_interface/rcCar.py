798fe069ca020a38435d76b38fb2908299e8f29b
Daniel Henry <email378637@gmail.com>
2017-10-05 20:39:55 -0700

    Update rcCar.py

--- robotControl/python_interface/rcCar.py
+++ robotControl/python_interface/rcCar.py
@@ -10,28 +10,38 @@ except Exception as e:
     print(e)
     
 def sendCode(code, delay=0):
-    conn.write(code.encode())
+    byteChr = chr(int(code, 2))
+    conn.write(byteChr.encode())
     time.sleep(delay)
     return conn.readline().decode()
 
 def keyState(keys):
     stack = []
     for key in keys:
-        stack.append([win32api.GetAsyncKeyState(ord(key)), key])
+        state = win32api.GetAsyncKeyState(ord(key))
+        if(state < 0 or state == 1):
+            stack.append(key.lower())
     return stack
-    
+
 def main():
-    motors_on = False
+    motorCode = {'w': '000', 'a': '110', 's': '100', 'd': '111'} 
+    motors = {'w': False, 'a': False, 's': False, 'd': False}
     while True:
-        for state, key in keyState('WASD'): 
-            if(state < 0 or state == 1):
-                result = sendCode(key.lower())
-                motors_on = True
-                print('key: {}, value: {}, Pin: {}'.format(key, state, result))
-                break
+        keyStack = keyState('WASD')
+        if(keyStack):
+            for key, value in motors.items():
+                if(value):
+                    if key not in keyStack:
+                        motors[key] = False
+                        print('off: ', sendCode('0' + motorCode[key]))
+                else:
+                    if key in keyStack:
+                        motors[key] = True
+                        print('on: ', sendCode('1' + motorCode[key]))
         else:
-            if(motors_on):
-                results = sendCode('q')
-                motors_on = False
-                print(results)      
+            for key, value in motors.items():
+                if(value):
+                    motors[key] = False
+                    print('off: ', sendCode('0' + motorCode[key]))
+                                 
 main()
