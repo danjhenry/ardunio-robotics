eca577554cadced5a5e18191c5a02024934363ea
Daniel Henry <email378637@gmail.com>
2017-10-07 19:40:14 -0700

    Update rc_Logic.ino

--- robotControl/rc_logic/rc_Logic.ino
+++ robotControl/rc_logic/rc_Logic.ino
@@ -1,14 +1,6 @@
 #include <Arduino.h>
 
-String forward = "000";
-String backward = "100";
-String left = "110";
-String right = "111";
-
-const int forward_pin = 2;
-const int backward_pin = 3;
-const int right_pin = 4;
-const int left_pin = 5;
+const int pins[4] = {5, 4, 3, 2};
 
 void setup() {
   Serial.begin(9600);
@@ -20,21 +12,9 @@ void loop() {
   char input;
   if (Serial.available() > 0) {
     input = Serial.read();
-    for (int i = 3; i--;) {
-      motorCode += String((input >> i) & 0x01);
-    }
-    state = (input >> 3) & 0x01;
-    if (motorCode == forward) {
-      motor(state, forward_pin);
-    }
-    else if (motorCode == backward) {
-      motor(state, backward_pin);
-    }
-    else if (motorCode == left) {
-      motor(state, left_pin);
-    }
-    else if (motorCode == right) {
-      motor(state, right_pin);
+    for (int i = 4; i--;) {
+      state = (input >> i) & 0x01;
+      motor(state, pins[i]);
     }
   }
 }
@@ -42,10 +22,8 @@ void loop() {
 void motor(bool state, int pin) {
   if (state) {
     digitalWrite(pin, HIGH);
-    Serial.println(pin);
   }
   else {
     digitalWrite(pin, LOW);
-    Serial.println(pin);
   }
 }
