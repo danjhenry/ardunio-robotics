f136c151f6d2556b8f0c6ef71521e2d141bef059
Daniel Henry <email378637@gmail.com>
2017-10-27 08:11:12 -0700

    Update rcCar.py

--- robotControl/python_interface/rcCar.py
+++ robotControl/python_interface/rcCar.py
@@ -2,18 +2,18 @@ import serial, time
 from random import randint
 import win32api
 import win32con
-
+# tries a connection to the ardunio through windows communication port 3.
 try:
     conn = serial.Serial('COM3', 9600, timeout=.1)
     time.sleep(2)
 except Exception as e:
     print(e)
-    
+# sends a custom bit code to the ardunio.    
 def sendCode(code, delay=0):
     byteChr = chr(int(code, 2))
     conn.write(byteChr.encode())
     time.sleep(delay)
-
+# checks for a set of keys being pressed or not using windows api and returns the keys pressed in lowercase form.
 def keyState(keys):
     stack = []
     for key in keys:
@@ -21,7 +21,7 @@ def keyState(keys):
         if(state < 0 or state == 1):
             stack.append(key.lower())
     return stack
-
+# creates and send a bit code only if a change happens.
 def main():
     names = ('forward', 'backward', 'right', 'left')
     motors = ('w', 's', 'd', 'a')
