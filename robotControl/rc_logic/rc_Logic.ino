f5dbd5db4ee6c36476e311249016f076a3bdd676
Daniel Henry <email378637@gmail.com>
2017-10-27 08:05:47 -0700

    Update rc_Logic.ino

--- robotControl/rc_logic/rc_Logic.ino
+++ robotControl/rc_logic/rc_Logic.ino
@@ -9,9 +9,11 @@ void setup() {
 void loop() {
   bool state;
   String motorCode;
+  // 4 bit  one hot code is sent to the ardunio
   char input;
   if (Serial.available() > 0) {
     input = Serial.read();
+    // 4 bit code is read using bit shift then sends either on or off command to each pin. (One Hot encoding)
     for (int i = 4; i--;) {
       state = (input >> i) & 0x01;
       motor(state, pins[i]);
