3d75c2b423c68980b1ea490aafab948d080abbec
Daniel Henry <email378637@gmail.com>
2018-01-09 14:27:03 -0800

    Update wifi_tracks.ino

--- NetworkRC/wifi_tracks/wifi_tracks.ino
+++ NetworkRC/wifi_tracks/wifi_tracks.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "QuantiumLink";
-const char* password = "HPGNLWZ2DNTXB5K7";
+const char* ssid = "";
+const char* password = "";
 
 ESP8266WebServer server(80);
 
